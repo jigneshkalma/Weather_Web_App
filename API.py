f85c68b47cae529d00f9bbfc62f4989b6386da10
jigneshkalma <106331437+jigneshkalma@users.noreply.github.com>
2024-06-11 16:34:42 +0530

    Update API.py

--- API.py
+++ API.py
@@ -1,3 +1,3 @@
-API_Key = "04f497a11ec6d9613b6591a06a03ae10"
+API_Key = "Enter Your API Key"
 base_url_for_current_weather = "/data/2.5/weather"
 base_url_for_weather_forecast = "/data/2.5/forecast"
